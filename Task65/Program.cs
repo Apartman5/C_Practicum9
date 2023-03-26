97040af0c96f1e7c97c85db540a6ba61fb5873f0
Igor Kazakov <kiw@lefterova.ru>
2023-03-26 20:10:45 +0500

    first commit

---
+++ Task65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 65: 
+// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m > n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+
+string NaturalNumbers (int m, int n)
+{
+    string end = " ";
+    Console.Write(m + " ");
+    if (m < n)
+        return NaturalNumbers(m + 1, n);
+    else return end ;
+}
+
+NaturalNumbers(m, n);
